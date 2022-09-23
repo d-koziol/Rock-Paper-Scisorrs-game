29f368a74d4af476f502ec8e292ca295cbbfdab7
Damian <d.p.koziol@gmail.com>
2022-09-24 00:10:05 +0200

    Function playRoud updated. Else if statement added.

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 function getComputerChoice() {
-    const weapons = ["Rock", "Paper", "Scissors"];
+    const weapons = ["ROCK", "PAPER", "SCISSORS"];
     return weapons[Math.floor(Math.random() * weapons.length)];
 }
-console.log(getComputerChoice());
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log(`Tie game! You choose: ${playerSelection}`);
+        return playRound(0, 0);
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        return playRound(1, 0);
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        return playRound(1, 0);
 
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        return playRound(1, 0);
 
+    } else {
+        console.log(`You lost.. ${computerSelection} beats ${playerSelection}`);
+        return playRound(0, 1);
 
+    }
+  }
+   
+  const playerSelection = prompt("Choose your weapon(rock, paper or scissors)").toLocaleUpperCase();
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
 
