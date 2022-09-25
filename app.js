6379b86cf0b5b66c452396fd0e995e890066a058
Damian <d.p.koziol@gmail.com>
2022-09-25 19:04:02 +0200

    Score counter added

--- app.js
+++ app.js
@@ -3,29 +3,34 @@ function getComputerChoice() {
     return weapons[Math.floor(Math.random() * weapons.length)];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log(`Tie game! You choose: ${playerSelection}`);
-        return playRound(0, 0);
+        
     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
         console.log(`You won! ${playerSelection} beats ${computerSelection}`);
-        return playRound(1, 0);
+        playerScore++;
+
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
         console.log(`You won! ${playerSelection} beats ${computerSelection}`);
-        return playRound(1, 0);
+        playerScore++;
 
     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
         console.log(`You won! ${playerSelection} beats ${computerSelection}`);
-        return playRound(1, 0);
+        playerScore++;
 
     } else {
         console.log(`You lost.. ${computerSelection} beats ${playerSelection}`);
-        return playRound(0, 1);
+        computerScore++;
 
     }
   }
    
   const playerSelection = prompt("Choose your weapon(rock, paper or scissors)").toLocaleUpperCase();
   const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+  playRound(playerSelection, computerSelection);
+  console.log(`Your score: ${playerScore} | computer score ${computerScore}`);
 
