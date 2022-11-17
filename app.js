3cdea843e6cd36f10afbcb65a1e4331f7120a830
Damian Kozioł <d.p.koziol@gmail.com>
2022-11-17 12:46:44 +0100

    PlayRound() function updated.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let playerScore = 0;  // Variables declaration.
 let computerScore = 0;
 let computerSelection;
 let playerSelection;
+let roundCounter = 0;
+const maxRound = 5;
 
 
 
@@ -13,11 +15,12 @@ function getComputerChoice() {  // Function that generates random computer's wea
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-    
+    button.addEventListener("click", () => {
+        
         if(button.id === "rock_btn") {
             playerSelection = "ROCK";
             playRound(playerSelection);
+   
         };
         if(button.id === "paper_btn") {
             playerSelection = "PAPER";
@@ -36,47 +39,43 @@ function playRound(playerSelection) {  // Function  that allows to play one roun
     computerSelection = getComputerChoice();
     const pScore = document.querySelector("#playerScore");
     const cScore = document.querySelector("#computerScore");
-    const rScore = document.querySelector("#roundResult");
+    const rResult = document.querySelector("#roundResult");
+    const rNumber = document.querySelector("#roundNumber");
     
     
     if (playerSelection === computerSelection) {
-        
-            rScore.textContent = `It's a tie... Try again.`;
-            return 0;
+        rNumber.textContent = roundCounter += 1;
+        rResult.textContent = `It's a tie... Try again.`;
+            
         }
         else if( (playerSelection === "ROCK" && computerSelection === "SCISSORS") || 
-                 (playerSelection === "PAPER" && computerSelection ==="ROCK") || 
-                 (playerSelection === "SCISSORS" && computerSelection === "PAPER") ) 
+        (playerSelection === "PAPER" && computerSelection ==="ROCK") || 
+        (playerSelection === "SCISSORS" && computerSelection === "PAPER") ) 
         {
-            rScore.textContent = `You Win This Round! ${playerSelection} beats ${computerSelection}.`;
+            rResult.textContent = `You Win This Round! ${playerSelection} beats ${computerSelection}.`;
             pScore.textContent = parseInt(pScore.textContent) + 1;
-            return 1;
+            rNumber.textContent = roundCounter += 1;
         }
         else 
         {
-            rScore.textContent = `You Lose This Round! ${computerSelection} beats ${playerSelection}.`;
+            rResult.textContent = `You Lose This Round! ${computerSelection} beats ${playerSelection}.`;
             cScore.textContent = parseInt(cScore.textContent) + 1;
-            return -1;
+            rNumber.textContent = roundCounter += 1;
         }
+        return roundCounter;
     }
 
-function game(playerScore, computerScore) {   // Function that allows to play 5 rounds.
-    if (playerScore = 5){
-        console.log(`You won! The result is ${playerScore} to ${computerScore}` );
-    } else if (computerScore = 5){
-        console.log(`Computer won! The result is ${computerScore} to ${playerScore}` );
+function endGame(playerScore, computerScore) {
+        const gResult = document.querySelector("#gameResult");
+        if (playerScore > computerScore && roundCounter === maxRound) {
+            gResult.textContent = `You won this game with score ${playerScore} to ${computerScore}`;
+        } 
+        else if (computerScore > playerScore && roundCounter === maxRound) {
+            gResult.textContent = `You lost this game with score ${playerScore} to ${computerScore}`;
+        }
+        else if (playerScore === computerScore && roundCounter === maxRound) {
+            gResult.textContent = `It's a tie ${playerScore} to ${computerScore}`;
+        }
+        console.log(gResult);
     }
-    }
-game(playerScore, computerScore);
-
-
-function getScore (playerScore, computerScore){ // Function that keeps score and reports it at the end of the game.
-    if (playerScore < computerScore) {
-        console.log(`Game over, computer wins with score ${computerScore}:${playerScore}`)
-    } else if ( playerScore > computerScore){
-        console.log(`Game over, You won with score ${playerScore}:${computerScore}`);
-    }
-}
-getScore (playerScore, computerScore);
-
-
+endGame();
